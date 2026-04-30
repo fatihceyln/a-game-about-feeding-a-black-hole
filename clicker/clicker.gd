486bde6cc2dddfbe993f0a51edaa658008b0607c
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-04-30 21:33:45 +0300

    Add rotation functionality to Clicker by introducing angle offset and speed control

--- clicker/clicker.gd
+++ clicker/clicker.gd
@@ -4,11 +4,20 @@ class_name Clicker
 const RADIUS: float = 100.0
 const SEGMENT_COUNT: int = 12
 const SEGMENT_RATIO: float = 0.7
+const SPEED: float = 0.6
+
+var angle_offset: float = 0.0
+
+
+func _process(delta: float) -> void:
+	angle_offset = wrapf(angle_offset + (delta * SPEED), 0.0, TAU)
+	queue_redraw()
+
 
 func _draw() -> void:
 	var segment_size: float = TAU / SEGMENT_COUNT
 
 	for i in SEGMENT_COUNT:
-		var start_angle: float = float(i) * segment_size
+		var start_angle: float = angle_offset + (float(i) * segment_size)
 		var end_angle: float = start_angle + (segment_size * SEGMENT_RATIO)
 		draw_arc(Vector2.ZERO, RADIUS, start_angle, end_angle, 20, Color.BLACK, 8)		
