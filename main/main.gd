28c9ac6ae07073303d6716a564ca825d676ddb18
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-04-30 21:09:07 +0300

    Add Main script to handle asteroid spawning around BlackHole

---
+++ main/main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+class_name Main
+
+const ASTEROID = preload("uid://db1ixbef0ki6")
+
+@export var asteroid_count: int = 35
+@export var spawn_radius_range: Vector2 = Vector2(160, 600)
+
+@onready var black_hole: BlackHole = $BlackHole
+
+func _ready() -> void:
+	for i in asteroid_count:
+		var asteroid: Asteroid = ASTEROID.instantiate()
+		var angle: float = TAU * float(i) / asteroid_count
+		var distance: float = randf_range(spawn_radius_range.x, spawn_radius_range.y)
+		asteroid.position = black_hole.position + (Vector2.from_angle(angle) * distance)
+		add_child(asteroid)
