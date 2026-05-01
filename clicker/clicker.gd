2b712110510fc4785f3971fa2dfe21cadb5fc45e
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-05-01 16:01:45 +0300

    Add speed boost functionality to Clicker and enhance hit animation with speed tweening

--- clicker/clicker.gd
+++ clicker/clicker.gd
@@ -7,6 +7,7 @@ const SEGMENT_ARC_RATIO: float = 0.7
 const ROTATION_SPEED: float = 0.6
 
 var angle_offset: float = 0
+var speed_boost: float = 0.0
 
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
@@ -17,7 +18,7 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	angle_offset = wrapf(angle_offset + (ROTATION_SPEED * delta), 0, TAU)
+	angle_offset = wrapf(angle_offset + (delta * (ROTATION_SPEED + speed_boost)), 0.0, TAU)
 	queue_redraw()
 
 
@@ -30,8 +31,14 @@ func _draw() -> void:
 
 
 func play_hit_animation() -> void:
-	var tween: Tween = create_tween()
-	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.1)\
+	var scale_tween: Tween = create_tween()
+	scale_tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.1)\
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
-	tween.tween_property(self, "scale", Vector2.ONE, 0.1)\
+	scale_tween.tween_property(self, "scale", Vector2.ONE, 0.1)\
 		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)
+
+	var speed_tween: Tween = create_tween()
+	speed_tween.tween_property(self, "speed_boost", 2, 0.1)\
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)
+	speed_tween.tween_property(self, "speed_boost", 0.0, 0.2)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
