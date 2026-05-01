257269b35d4ff8b71afccce020829ec0bb25aa7d
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-05-01 16:39:47 +0300

    Add session timer functionality to main scene, allowing for game duration tracking and game over condition

--- main/main.gd
+++ main/main.gd
@@ -5,12 +5,18 @@ const ASTEROID: PackedScene = preload("uid://db1ixbef0ki6")
 
 @export var asteroid_count: int = 35
 @export var spawn_radius_range: Vector2 = Vector2(160, 600)
+@export var session_duration: float = 20
 
 @onready var black_hole: BlackHole = $BlackHole
 @onready var clicker: Clicker = $Clicker
 @onready var clicker_timer: Timer = $ClickerTimer
+@onready var session_timer: Timer = $SessionTimer
 @onready var start_screen: Control = %StartScreen
 @onready var timer_label: Label = %TimerLabel
+
+
+var session_time: float = 0
+
 
 func _ready() -> void:
 	timer_label.visible = false
@@ -18,6 +24,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	clicker.global_position = get_global_mouse_position()
+
 
 func spawn_asteroids() -> void:
 	for i: int in asteroid_count:
@@ -42,3 +49,15 @@ func _on_start_button_pressed() -> void:
 	start_screen.visible = false
 	spawn_asteroids()
 	clicker_timer.start()
+	
+	session_time = session_duration
+	session_timer.start()
+
+
+func _on_session_timer_timeout() -> void:
+	session_time -= session_timer.wait_time
+	timer_label.text = "%.1f" % session_time
+	if session_time <= 0:
+		print("game over")
+		session_timer.stop()
+		clicker_timer.stop()
