e36ee1cd40eadb0420b1154993afa89cef008509
Fatih <45256657+fatihceyln@users.noreply.github.com>
2026-05-01 14:24:20 +0300

    Add outline to Asteroid node with customizable properties

--- asteroid/asteroid.gd
+++ asteroid/asteroid.gd
@@ -5,12 +5,22 @@ const BASE_COLOR: Color = Color(0.654, 0.397, 0.413, 1.0)
 const POINT_COUNT_RANGE: Vector2i = Vector2i(7, 11)
 const RADIUS_RATIO_RANGE: Vector2 = Vector2(0.85, 1.15)
 
+const OUTLINE_COLOR: Color = Color(0.595, 0.352, 0.368, 1.0)
+const OUTLINE_WIDTH: float = 3
+
 @export var radius: float = 40.0
 @onready var shape: Polygon2D = $Shape
+@onready var outline: Line2D = $Outline
 
 func _ready() -> void:
-	shape.polygon = make_polygon_points()
+	var points: PackedVector2Array = make_polygon_points()
+	shape.polygon = points
 	shape.color = BASE_COLOR
+	
+	outline.points = points
+	outline.default_color = OUTLINE_COLOR
+	outline.closed = true
+	outline.width = OUTLINE_WIDTH
 
 func make_polygon_points() -> PackedVector2Array:
 	var point_count: int = randi_range(POINT_COUNT_RANGE.x, POINT_COUNT_RANGE.y)
